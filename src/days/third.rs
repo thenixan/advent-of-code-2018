51ec6ac05d145de60acf264e3ae45d50b900311c
Ilya Nixan <thenixan@gmail.com>
2018-12-06 21:08:47 +0300

    Day 3 task 1 done

--- src/days/third.rs
+++ src/days/third.rs
@@ -1,4 +1,6 @@
-use std::num::ParseIntError;
+use std::collections::HashMap;
+use std::fmt;
+use std::fmt::Formatter;
 use std::str::FromStr;
 
 use days::print_header;
@@ -6,10 +8,26 @@ use days::read_file_to_vec;
 
 pub fn run_first_task() {
     print_header(3, 1);
-    let a = read_file_to_vec("days/3/input", |s| {
+    match read_file_to_vec("days/3/input", |s| {
         s.parse::<Tile>()
-    });
+    }).map(|tiles| {
+        tiles.into_iter().fold(&mut HashMap::new(), |plot, item| {
+            for i in item.padding_left..item.padding_left + item.size.width {
+                for j in item.padding_top..item.padding_top + item.size.height {
+                    let val = plot.entry((i, j)).or_insert_with(|| 0);
+                    *val += 1;
+                }
+            }
+            plot
+        }).to_owned()
+    }).map(|plot| {
+        plot.into_iter().filter(|(_, v)| *v > 1).count()
+    }) {
+        Ok(x) => println!("Result: {}", x),
+        Err(e) => println!("{}", e),
+    };
 }
+
 
 struct Size {
     height: i32,
@@ -20,9 +38,25 @@ impl Size {
     fn new(height: i32, width: i32) -> Size {
         Size { height, width }
     }
+}
 
-    fn square(&self) -> i32 {
-        self.width * self.height
+impl fmt::Display for Size {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), std::fmt::Error> {
+        write!(f, "(h: {}, w: {}", self.height, self.width)
+    }
+}
+
+impl FromStr for Size {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, <Self as FromStr>::Err> {
+        let parts: Vec<&str> = s.split(|c| c == 'x').collect();
+        let width = parts[0].parse::<i32>();
+        let height = parts[1].parse::<i32>();
+        match (width, height) {
+            (Ok(w), Ok(h)) => Ok(Size::new(h, w)),
+            _ => Err("error parsing sizes".to_string()),
+        }
     }
 }
 
@@ -39,13 +73,30 @@ impl Tile {
     }
 }
 
+impl fmt::Display for Tile {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), std::fmt::Error> {
+        write!(f, "id: {}, size: {}, left: {}, top: {}", self.id, self.size, self.padding_left, self.padding_top)
+    }
+}
+
 impl FromStr for Tile {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Tile, Self::Err> {
-        let parts = s.split_whitespace().collect::<Vec<_>>();
-        let id = parts[0].chars().skip(1).collect::<&str>();
-        let paddings = parts[2].split(',');
-        unimplemented!()
+        let parts: Vec<&str> = s.split_whitespace().collect();
+        let id: &str = &parts[0][1..];
+        let paddings: Vec<&str> = parts[2].split(|c| c == ',' || c == ':').collect();
+        let padding_left = paddings[0].parse::<i32>();
+        let padding_top = paddings[1].parse::<i32>();
+        match (padding_left, padding_top) {
+            (Ok(left), Ok(top)) => {
+                let size = parts[3].parse::<Size>();
+                match size {
+                    Ok(s) => Ok(Tile::new(id.to_string(), left, top, s)),
+                    Err(e) => Err(e)
+                }
+            }
+            _ => Err("cannot parse paddings".to_string()),
+        }
     }
 }
